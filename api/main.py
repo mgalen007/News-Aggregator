31a32f0353ee444c0b349abbeccab6ea49377ee6
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-03-28 20:29:40 +0200

    Added tags for documentation

--- api/main.py
+++ api/main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 from routers import finance, news, politics, sports, tech
 
 # Set up the server
-app = FastAPI()
+app = FastAPI(
+    title='News Aggregator API',
+    description='This API gets the latest news from news sites',
+    version='1.0.0'
+)
 
 # Health check endpoint
-@app.get('/health-check')
+@app.get('/health-check', tags=['Health-check'])
 def health_check():
     return {
         "status": "ok",
@@ -13,8 +17,28 @@ def health_check():
     }
 
 # Add different routers for the endpoints
-app.include_router(news.router, prefix='/api/news')
-app.include_router(tech.router, prefix='/api/tech')
-app.include_router(finance.router, prefix='/api/finance')
-app.include_router(politics.router, prefix='/api/politics')
-app.include_router(sports.router, prefix='/api/sports')
+app.include_router(
+    router=news.router,
+    prefix='/api/news',
+    tags=['News']
+)
+app.include_router(
+    router=tech.router,
+    prefix='/api/tech',
+    tags=['Tech']
+)
+app.include_router(
+    router=finance.router,
+    prefix='/api/finance',
+    tags=['Finance']
+)
+app.include_router(
+    router=politics.router,
+    prefix='/api/politics',
+    tags=['Politics']
+)
+app.include_router(
+    router=sports.router,
+    prefix='/api/sports',
+    tags=['Sports']
+)
